087941bf30bf0d145458cfedbf21e2a02323dd45
Vaishnavi Satpute <vaishnavisatpute195@gmail.com>
2026-07-15 23:39:28 +0530

    Postgde developed

---
+++ backend-java/backend-java/src/main/java/com/skillbridge/backend_java/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.skillbridge.backend_java.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "SkillBridge Java Backend Running Successfully!";
+    }
+}
